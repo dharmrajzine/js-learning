fe5a0828ffb3fc754b135918d883fb7f6a1f1291
Dharmraj D. Zine <96525634+rajjzine@users.noreply.github.com>
2024-04-30 09:21:02 +0000

    discussion on variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 1445533
+let accountEmail = "dharmrajzine@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+//accountId = 2 //not allowed
+
+accountEmail="hdfc.com"
+accountPassword="212121212"
+accountCity="Bangaluru"
+
+console.log(accountId)
+
+/*
+prefer not to use var
+because of issue in block space and functional scope
+ */
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+//varirable types
+//numbers, string, Booleans, Arrays, Objects, Dynamic typing
